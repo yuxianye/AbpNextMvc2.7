bf713efcddbaf3aa280946b5b5c13dfc67016427
yuxianye <yuxianye@163.com>
2020-05-09 16:20:57 +0800

    原版

---
+++ aspnet-core/src/Solution.Web/Pages/SolutionPage.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.Localization;
+using Microsoft.AspNetCore.Mvc.Razor.Internal;
+using Solution.Localization;
+using Volo.Abp.AspNetCore.Mvc.UI.RazorPages;
+
+namespace Solution.Web.Pages
+{
+    /* Inherit your UI Pages from this class. To do that, add this line to your Pages (.cshtml files under the Page folder):
+     * @inherits Solution.Web.Pages.SolutionPage
+     */
+    public abstract class SolutionPage : AbpPage
+    {
+        [RazorInject]
+        public IHtmlLocalizer<SolutionResource> L { get; set; }
+    }
+}
